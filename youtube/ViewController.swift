2746147f2328e1fb1bc50fc0f48a685bc8016763
Vina Melody <vina_melody@yahoo.com>
2016-11-13 20:44:06 +0800

    Clean and refactor the extensions

--- youtube/ViewController.swift
+++ youtube/ViewController.swift
@@ -147,20 +147,4 @@ class VideoCell: UICollectionViewCell {
 }
 
 
-extension UIView {
-    
-    func addConstraintsWithFormat(format: String, views: UIView...) {
-        
-        var viewsDictionary  = [String: UIView]()
-        
-        for(index, view) in views.enumerated() {
-            let key = "v\(index)"
-            view.translatesAutoresizingMaskIntoConstraints = false
-            viewsDictionary[key] = view
-        }
-        
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
-        
-        
-    }
-}
+
