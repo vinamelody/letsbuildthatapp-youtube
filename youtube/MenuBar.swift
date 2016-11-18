8f6a901a91f9c87588f911addfbfa197c9c522ba
Vina Melody <vina_melody@yahoo.com>
2016-11-18 22:23:15 +0800

    Start implementing the collection view for MenuBar

--- youtube/MenuBar.swift
+++ youtube/MenuBar.swift
@@ -10,13 +10,46 @@ import UIKit
 
 class MenuBar: UIView {
     
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.rgb(red: 230, green: 32, blue: 31)
+        
+        // conforming the UICollectionViewDataSource extension, but don't forget to add lazy var
+        cv.dataSource = self
+        // conforming the UICollectionViewDelegate
+        cv.delegate = self
+        return cv
+    }()
+    
+    let cellId = "cellId"
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = UIColor.rgb(red: 230, green: 32, blue: 31)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
+        addSubview(collectionView)
+        addConstraintsWithFormat(format: "H:|[v0]|", views: collectionView)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: collectionView)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MenuBar: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = UIColor.blue
+        
+        return cell
     }
 }
