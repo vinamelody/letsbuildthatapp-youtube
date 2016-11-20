58d1be326a76f770df2e9d2c782e077757e3b9cb
Vina Melody <vina_melody@yahoo.com>
2016-11-20 22:50:37 +0800

    Map more json to video array

--- youtube/HomeController.swift
+++ youtube/HomeController.swift
@@ -49,9 +49,26 @@ class HomeController: UICollectionViewController {
                 // this would create the dictionary type for variable json
                 let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
                 
+                self.videos = [Video]()
+                
                 for dictionary in json as! [[String: AnyObject]] {
-                    print(dictionary["title"])
+                    
+                    let video = Video()
+                    video.title = dictionary["title"] as? String
+                    video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
+                    
+                    let channelDictionary = dictionary["channel"] as! [String: AnyObject]
+                    
+                    let channel = Channel()
+                    channel.name = channelDictionary["name"] as? String
+                    channel.profileImageName = channelDictionary["profile_image_name"] as? String
+                    
+                    video.channel = channel
+                    
+                    self.videos?.append(video)
                 }
+                
+                self.collectionView?.reloadData()
                 
             } catch let jsonError {
                 print(jsonError)
