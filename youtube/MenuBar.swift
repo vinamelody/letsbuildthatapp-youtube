9e1c67e992d3f7dd72e17d46812939d89ef01262
Vina Melody <vina_melody@yahoo.com>
2016-12-25 21:10:42 +0800

    Animating the white line movement on didSelectItem

--- youtube/MenuBar.swift
+++ youtube/MenuBar.swift
@@ -46,6 +46,8 @@ class MenuBar: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    var horizontalBarLeftAnchorConstraint: NSLayoutConstraint?
+    
     func setupHorizontalBar() {
         let horizontalBarView = UIView()
         horizontalBarView.backgroundColor = UIColor(white: 0.95, alpha: 1)
@@ -55,11 +57,24 @@ class MenuBar: UIView {
         // the old school way of doing things
         // horizontalBarView.frame = CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
         
+        horizontalBarLeftAnchorConstraint = horizontalBarView.leftAnchor.constraint(equalTo: self.leftAnchor)
+        horizontalBarLeftAnchorConstraint?.isActive = true
+        
         // the new way since ios9, need x,y, width, height
-        horizontalBarView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         horizontalBarView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         horizontalBarView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1/4).isActive = true
         horizontalBarView.heightAnchor.constraint(equalToConstant: 4).isActive = true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let x = CGFloat(indexPath.item) * frame.width / 4
+        horizontalBarLeftAnchorConstraint?.constant = x
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.layoutIfNeeded()
+        }
+        ,completion: nil)
     }
 }
 
