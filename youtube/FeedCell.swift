c837e3cd9735893a560251b724db38c5e40360ac
Vina Melody <vina_melody@yahoo.com>
2016-12-26 15:37:18 +0800

    Initializing collectionView

--- youtube/FeedCell.swift
+++ youtube/FeedCell.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class FeedCell: BaseCell {
     
-    let collectionView: UICollectionView = {
+    lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = .white
+        cv.delegate = self
+        cv.dataSource = self
         return cv
     }()
+    
+    let cellId = "cellId"
     
     override func setupViews() {
         super.setupViews()
@@ -24,5 +28,21 @@ class FeedCell: BaseCell {
         addConstraintsWithFormat(format: "H:|[v0]|", views: collectionView)
         addConstraintsWithFormat(format: "V:|[v0]|", views: collectionView)
         
+        collectionView.register(VideoCell.self, forCellWithReuseIdentifier: cellId)
+        
     }
 }
+
+extension FeedCell: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
+}
+
