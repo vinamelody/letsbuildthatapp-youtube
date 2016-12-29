7b618f0ac89405f029eb87c6f96ce1d0b17525ec
Vina Melody <vina_melody@yahoo.com>
2016-12-29 21:05:45 +0800

    Refactor ApiService

--- youtube/ApiService.swift
+++ youtube/ApiService.swift
@@ -12,11 +12,11 @@ import UIKit
 class ApiService: NSObject {
     
     static let sharedInstance = ApiService()
+    let baseUrl = "https://s3-us-west-2.amazonaws.com/youtubeassets"
     
-    func fetchSubscriptionFeed(completion: @escaping ([Video]) -> ()) {
+    func fetchFeedForUrlString(urlString: String, completion: @escaping ([Video]) -> ()) {
         
-        let url = URL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/subscriptions.json")
-        
+        let url = URL(string: urlString)
         URLSession.shared.dataTask(with: url!) { (data, response, error) in
             
             if error != nil {
@@ -57,100 +57,26 @@ class ApiService: NSObject {
             }
             
             }.resume()
-        
+    }
+    
+    func fetchSubscriptionFeed(completion: @escaping ([Video]) -> ()) {
+        fetchFeedForUrlString(urlString: "\(baseUrl)/subscriptions.json") { (videos) in
+            completion(videos)
+            
+        }
     }
     
     func fetchTrendingFeed(completion: @escaping ([Video]) -> ()) {
-        
-        let url = URL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/trending.json")
-        
-        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+        fetchFeedForUrlString(urlString: "\(baseUrl)/trending.json") { (videos) in
+            completion(videos)
             
-            if error != nil {
-                print(error)
-                return
-            }
-            
-            do {
-                // this would create the dictionary type for variable json
-                let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
-                
-                var videos = [Video]()
-                
-                for dictionary in json as! [[String: AnyObject]] {
-                    
-                    let video = Video()
-                    video.title = dictionary["title"] as? String
-                    video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
-                    
-                    let channelDictionary = dictionary["channel"] as! [String: AnyObject]
-                    
-                    let channel = Channel()
-                    channel.name = channelDictionary["name"] as? String
-                    channel.profileImageName = channelDictionary["profile_image_name"] as? String
-                    
-                    video.channel = channel
-                    
-                    videos.append(video)
-                }
-                
-                DispatchQueue.main.async {
-                    completion(videos)
-                }
-                
-                
-            } catch let jsonError {
-                print(jsonError)
-            }
-            
-            }.resume()
-        
+        }
     }
     
     func fetchVideos(completion: @escaping ([Video]) -> ()) {
-        
-        let url = URL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json")
-        
-        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+        fetchFeedForUrlString(urlString: "\(baseUrl)/home.json") { (videos) in
+            completion(videos)
             
-            if error != nil {
-                print(error)
-                return
-            }
-            
-            do {
-                // this would create the dictionary type for variable json
-                let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
-                
-                var videos = [Video]()
-                
-                for dictionary in json as! [[String: AnyObject]] {
-                    
-                    let video = Video()
-                    video.title = dictionary["title"] as? String
-                    video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
-                    
-                    let channelDictionary = dictionary["channel"] as! [String: AnyObject]
-                    
-                    let channel = Channel()
-                    channel.name = channelDictionary["name"] as? String
-                    channel.profileImageName = channelDictionary["profile_image_name"] as? String
-                    
-                    video.channel = channel
-                    
-                    videos.append(video)
-                }
-                
-                DispatchQueue.main.async {
-                    completion(videos)
-                }
-                
-                
-            } catch let jsonError {
-                print(jsonError)
-            }
-            
-            }.resume()
-        
+        }
     }
 }
